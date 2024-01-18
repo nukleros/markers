7e01e6767a096698cb6b44c55e06a422a0874e8c
Dustin Scott <dscott@redhat.com>
2024-01-18 17:29:16 -0600

    feat: add Define to top-level package
    Signed-off-by: Dustin Scott <dscott@redhat.com>

--- markers.go
+++ markers.go
@@ -7,3 +7,7 @@ import "github.com/nukleros/markers/marker"
 func NewRegistry() *marker.Registry {
 	return marker.NewRegistry()
 }
+
+func Define(name string, outputType interface{}) (*marker.Definition, error) {
+	return marker.Define(name, outputType)
+}
